ec63e1639370d3b44b22d8b4bf11cacc435f2c0a
Pawan <respropawan@gmail.com>
2026-03-12 22:58:09 +0530

    Added React frontend

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span className='text-muted'>Copyright 2026 @Pawan</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
